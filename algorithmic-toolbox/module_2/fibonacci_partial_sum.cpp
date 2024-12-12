cb15fef15834b20ef2424b90ad7d1dfe69289350
Do Pham Bao Hoang <franlkinclinton@gmail.com>
2024-12-13 05:22:06 +0700

    update

--- algorithmic-toolbox/module_2/fibonacci_partial_sum.cpp
+++ algorithmic-toolbox/module_2/fibonacci_partial_sum.cpp
@@ -6,7 +6,7 @@ vector<int> pisanoPeriod() {
     vector<int> period(60);
     period[0] = 0;
     period[1] = 1;
-    for (int i=2;i<60;i++)
+    for (int i = 2; i < 60; i++)
         period[i] = (period[i-1] + period[i-2])%10;
     return period;
 }
@@ -16,20 +16,26 @@ int fibLastDigitPartialSum(long long m, long long n) {
     vector<int> period = pisanoPeriod();
     m%=60;
     n%=60;
-	for (long long i=m;i<=n;i++)
-        sum+=period[i];
+    if (m<=n) {
+        for (long long i=m;i<=n;i++)
+            sum+=period[i];
+    } else {
+        for (long long i=m;i<60;i++)
+            sum+=period[i];
+        for (long long i=0;i<=n;i++)
+            sum+=period[i];
+    }
     return sum%10;
 }
 
 void solve() {
-    long long m,n;
+    long long m, n;
     cin >> m >> n;
-    cout << fibLastDigitPartialSum(m,n);
+    cout << fibLastDigitPartialSum(m, n);
 }
 
 int main() {
-    int t=1;
-    // cin >> t;
+    int t = 1;
     while (t--) {
         solve();
     }
